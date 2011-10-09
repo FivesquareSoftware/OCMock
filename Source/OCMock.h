09da070f4469c148e3fe58ef45a2ee6798ce6808
John Clayton <jclayton@book.com>
2011-10-08 18:17:30 -0700

    changes to make building for iOS easier

--- Source/OCMock.h
+++ Source/OCMock.h
@@ -3,8 +3,20 @@
 //  Copyright (c) 2004-2008 by Mulle Kybernetik. See License file for details.
 //---------------------------------------------------------------------------------------
 
+#ifdef TARGET_OS_IPHONE
+
+#import "OCMockObject.h"
+#import "OCMockRecorder.h"
+#import "OCMConstraint.h"
+#import "OCMArg.h"
+#import "NSNotificationCenter+OCMAdditions.h"
+
+#else
+
 #import <OCMock/OCMockObject.h>
 #import <OCMock/OCMockRecorder.h>
 #import <OCMock/OCMConstraint.h>
 #import <OCMock/OCMArg.h>
 #import <OCMock/NSNotificationCenter+OCMAdditions.h>
+
+#endif
